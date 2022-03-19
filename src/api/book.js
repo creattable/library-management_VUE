fc371f6a1fc24f92610a9e237c5147d3f523137f
QINJIALE <940403048@qq.com>
2022-03-19 14:16:57 +0800

    图书管理_图书列表

--- src/api/book.js
+++ src/api/book.js
@@ -9,3 +9,17 @@ export const getListApi = async(parm) =>{
 export const getCateListApi = async() =>{
     return await http.get("/api/category/cateList",null)
 }
+
+//新增图书
+export const addBookApi = async(parms) =>{
+    return await http.post("/api/books",parms)
+}
+
+//编辑
+export const editBookApi = async(parm) =>{
+    return await http.put("/api/books",parm)
+}
+//删除
+export const deleteBookApi = async(parm) =>{
+    return await http.delete("/api/books",parm)
+}
